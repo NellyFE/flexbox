98e2956ffed70ae4256f8dbb283f774d57f7a39f
Nelly tracy <makuochukwuemmanuela2@gmail.com>
2025-05-07 09:17:12 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,53 @@
+export const Home = () => {
+  return (
+    <div className="flex flex-wrap md:flex-nowrap mx-auto p-6 w-full h-[100vh] gap-8 justify-between">
+      {/* main div 1 */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2  grid-row-2 gap-2 grow-2">
+
+        <div className="w-full h-72  ">
+          <img
+            src="./src/assets/parfait.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        <div className="w-full h-72  ">
+          <img
+            src="./src/assets/parfait.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        <div className="w-full h-72  ">
+          <img
+            src="./src/assets/parfait.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        <div className="w-full h-72  ">
+          <img
+            src="./src/assets/parfait.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+
+{/* second container */}
+<div className="flex grow  items-stretch h-full">
+  <div className="w-full h-full  ">
+    <img
+      src="./src/assets/parfait.jpg"
+      alt=""
+      className="w-full h-full object-cover"
+    />
+  </div>
+</div>
+
+    </div>
+  );
+};
